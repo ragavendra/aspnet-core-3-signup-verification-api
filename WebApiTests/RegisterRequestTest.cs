a0eb1f141baa6013b8f3ae51c66912b424ac2d96
Ragavendra <raga@example.com>
2024-07-03 16:45:41 -0700

    added rfrsh tkn cntrlr test

--- WebApiTests/RegisterRequestTest.cs
+++ WebApiTests/RegisterRequestTest.cs
@@ -63,5 +63,48 @@ namespace WebApiTests
             Assert.Matches("abc@email.com", authResp_.Email);
             Assert.Matches("rfrshToken", authResp_.RefreshToken);
         }
+
+        [Fact]
+        public async Task RefreshToken()
+        {
+            // Arrange
+            // var authReq = new Mock<AuthenticateRequest>();
+            var authReq = new AuthenticateRequest();
+            var authResp = new AuthenticateResponse(){ Email = "abc@email.com", RefreshToken = "rfrshToken" };
+
+            var mockRepo = new Mock<IAccountService>();
+            var mockMapper = new Mock<IMapper>();
+
+            var httpContext = new DefaultHttpContext(); // or mock a `HttpContext`
+            httpContext.Request.Headers["token"] = "fake_token_here"; //Set header
+                                                                      //Controller needs a controller context
+
+            httpContext.Request.Headers["X-Forwarded-For"] = "";
+
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext,
+            };
+
+            mockRepo.Setup(repo => repo.RefreshToken(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(authResp);
+
+            var controller = new AccountsController(mockRepo.Object, mockMapper.Object) {
+                ControllerContext = controllerContext
+             };
+
+            // Act
+            var res = controller.RefreshToken();
+            // .Authenticate(authReq);
+
+            // Assert
+            var resp = Assert.IsType<OkObjectResult>(res.Result);
+
+            // var resp = Assert.IsType<ActionResult>(res);
+            var authResp_ = Assert.IsAssignableFrom<AuthenticateResponse>(resp.Value);
+
+            Assert.Matches("abc@email.com", authResp_.Email);
+            Assert.Matches("rfrshToken", authResp_.RefreshToken);
+        }
     }
 }
